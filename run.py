04b6bc0b5f29d33e78df0eafb0d5fa188eaedf15
Fatoye Babatunde <dayotunde25@gmail.com>
2025-09-09 02:26:51 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
 from __init__ import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-=======
 from __init__ import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
->>>>>>> 24806e06812e7a8caf53d6dd5ce6b2ee56a9d86f
