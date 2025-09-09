d844d59bc47fc9ab4cb5b3bf4c14d8c615cbfbd7
Fatoye Babatunde <dayotunde25@gmail.com>
2025-09-09 04:01:55 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from __init__ import create_app
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 locally
+    app.run(host="0.0.0.0", port=port, debug=True)
+
